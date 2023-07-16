d6336a8012dc0817803152c1751e4a1771b4c72c
olamide <oadams@fandm.edu>
2023-07-16 15:15:44 -0400

    complete

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,26 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
-    }
+    } // this is the constructor. it created a music object
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        int selectionIndex = -1;
+        for (int i = 0; i < playList.length; i++) {
+            // this checks to make sure the code is checking for the song collected
+            if (playList[i].equals(selection)) {
+                selectionIndex = i;
+            }
+
+        }
+        int forward = Math.abs(selectionIndex - startIndex);
+        int backward = playList.length - Math.abs(selectionIndex - startIndex);
+        if (forward < backward) {
+            return forward;
+        } else {
+            return backward;
+        }
+
     }
 }
